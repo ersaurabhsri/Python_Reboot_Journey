8ebcc63911885b24cd34eca4e7aa40da3948fa70
Saurabh <saurabh@tnpsfl.com>
2025-07-20 14:16:26 +0530

    Initial commit of restarting leanring

---
+++ Chapter-1/MultiLine_Print.py
@@ -0,0 +1,10 @@
+print('''
+    Hey folks!
+    I’m restarting my journey with Python from scratch after a break, 
+    and I’ve decided to document everything in this public repository.
+    This space will serve as my learning journal — filled with core concepts, hands-on exercises, notes, 
+    and mini-projects as I rebuild my foundation.
+    By keeping it open, I hope to stay consistent, track my progress, 
+    and possibly help others who are also learning or revisiting Python.
+    Whether you're a complete beginner or just refreshing your skills, feel free to explore, fork, or contribute!
+    ''')
