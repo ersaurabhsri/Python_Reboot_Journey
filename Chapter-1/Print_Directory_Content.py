ef3fc4630f807a07d519dc016ab6efbe0a66e823
Saurabh <saurabh@tnpsfl.com>
2025-07-21 11:46:31 +0530

    New Chapter added

--- Chapter-1/Print_Directory_Content.py
+++ Chapter-1/Print_Directory_Content.py
@@ -1,6 +1,6 @@
 import os
 
-directory_path="/"
+directory_path="D:\PythonRepos\Python_Reboot_Journey"
 
 contents=os.listdir(directory_path)
 for item in contents:
---
+++ Chapter-2/Variable.py
@@ -0,0 +1,10 @@
+#NOTE: In python datatype automatically identifies types of data
+
+a = 10  # a is integer
+b = 10  # b is also integer
+c = 12.5    # c is float types
+d = 0.23134 # d is float types
+e = "Hello Python" #e is String type and it will define in double quotes - ""
+print(a+b)
+print(c+d)
+print(e)
