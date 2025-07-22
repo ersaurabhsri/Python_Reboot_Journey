897cc264f598b0139d3b613f84f9fa2b07ad2d7a
Saurabh <saurabh@tnpsfl.com>
2025-07-22 22:07:21 +0530

    Added new chapter worked on string

---
+++ Chapter-3/01_intro_to_strings.py
@@ -0,0 +1,11 @@
+name = "Saurabh"
+lastname = "Shriwastav"
+
+nameshort = name[0:3] # start from index 0 all the way till 3 (excluding 3)
+print(nameshort)
+
+character1 = name[1]
+print(character1)
+
+last = lastname[2:6]
+print(last)
