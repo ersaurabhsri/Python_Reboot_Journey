b5b9bc6d9238cf795959ade51686d7e43b9a12a2
Saurabh <saurabh@tnpsfl.com>
2025-08-01 23:18:56 +0530

    Practiced and use of tuple method

--- Chapter-4/list_methods.py
+++ Chapter-4/list_methods.py
@@ -4,9 +4,15 @@ friends.append("Shubham")
 print(friends)
 
 l1 = [1, 34,62, 2, 6, 11]
-# l1.sort()
-# l1.reverse()
-# l1.insert(2, 333333) # Insert 333333 such that its index in the list is 3
-value = l1.pop(3)
-print(value)
+l1.sort()
 print(l1)
+l1.reverse()
+print(l1)
+l1.insert(2, 333333) # Insert 333333 such that its index in the list is 3
+print(l1)
+
+""" value = l1.pop(3)
+print(value)
+print(l1) 
+l1.remove(1) # Remove method will delete 1 from the list if it exists
+print(l1) """
