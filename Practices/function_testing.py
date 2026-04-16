a24711010c1fbf80afc9ea26e2d3a50ea189a15b
Saurabh Shriwastav <saurabh@tnpsfl.com>
2026-04-17 00:08:08 +0530

    Follow up on functions

---
+++ Practices/function_testing.py
@@ -0,0 +1,20 @@
+def hello_world():
+    print("Hello World")
+
+   # return 54
+
+#new = hello_world
+
+#a = new()
+
+#print(a)
+
+    def add_two_numbers(a,b):
+
+        c = a + b
+        return c
+
+    b = add_two_numbers(5,10)
+    print(b)
+hello_world()
+add_two_numbers()
